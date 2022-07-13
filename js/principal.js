6bb52455f34d5731d50732aedd57ae09797590cb
RubensStrassi <rubensstrassi@gmail.com>
2022-07-13 10:33:57 -0300

    04. Eventos, Formulários e Criando Element

--- js/principal.js
+++ js/principal.js
@@ -3,37 +3,70 @@ titulo.textContent = "Aparecida Nutricionista";
 
 let pacientes = document.querySelectorAll('.paciente');
 
-for (let i = 0; i < pacientes.length ; i++ ) {
+for (let i = 0; i < pacientes.length; i++) {
     let paciente = pacientes[i];
 
     let tdpeso = paciente.querySelector('.info-peso');
-let peso = tdpeso.textContent;
+    let peso = tdpeso.textContent;
 
-let tdaltura = paciente.querySelector('.info-altura');
-let altura = tdaltura.textContent;
+    let tdaltura = paciente.querySelector('.info-altura');
+    let altura = tdaltura.textContent;
 
-let tdimc = paciente.querySelector('.info-imc');
+    let tdimc = paciente.querySelector('.info-imc');
 
-let pesoEhValido = true;
-let alturaEhValido = true;
+    let pesoEhValido = true;
+    let alturaEhValido = true;
 
-if ( peso <= 0 || peso >= 1000 ) {
-    console.log("Peso inválido");
-    pesoEhValido = false;
-    tdimc.textContent = "Peso invalido";
-    paciente.classList.add("paciente-invalido");
+    if (peso <= 0 || peso >= 1000) {
+        console.log("Peso inválido");
+        pesoEhValido = false;
+        tdimc.textContent = "Peso invalido";
+        paciente.classList.add("paciente-invalido");
+    }
+
+    if (altura <= 0 || altura >= 3.0) {
+        console.log("Altura inválido");
+        alturaEhValido = false;
+        tdimc.textContent = "Altura invalido";
+        paciente.classList.add("paciente-invalido");
+    }
+
+    if (alturaEhValido && pesoEhValido) {
+        let imc = peso / (altura * altura);
+        tdimc.textContent = imc.toFixed(2);
+    }
 }
 
-if ( altura <= 0 || altura >= 3.0 ) {
-    console.log("Altura inválido");
-    alturaEhValido = false;
-    tdimc.textContent = "Altura invalido";
-    paciente.classList.add("paciente-invalido");
-}
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
+botaoAdicionar.addEventListener("click", function(event) {
+    event.preventDefault();
 
-if(alturaEhValido && pesoEhValido) {
-    let imc = peso /(altura*altura);
-    tdimc.textContent = imc.toFixed(2);
-}
+    var form = document.querySelector("#form-adiciona");
 
-}
+    var nome = form.nome.value;
+    var peso = form.peso.value;
+    var altura = form.altura.value;
+    var gordura = form.gordura.value;
+
+    var pacienteTr = document.createElement("tr");
+    var nomeTd = document.createElement("td");
+    var pesoTd = document.createElement("td");
+    var alturaTd = document.createElement("td");
+    var gorduraTd = document.createElement("td");
+    var imcTd = document.createElement("td");
+
+    nomeTd.textContent = nome;
+    pesoTd.textContent = peso;
+    alturaTd.textContent = altura;
+    gorduraTd.textContent = gordura;
+
+    pacienteTr.appendChild(nomeTd);
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(gorduraTd);
+
+    var tabela = document.querySelector("#tabela-pacientes");
+
+    tabela.appendChild(pacienteTr);
+});
+
